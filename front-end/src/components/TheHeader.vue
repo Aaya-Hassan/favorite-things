867c9cebcf38831288b390ff252d2014de35b428
Aaya-Hassan <aayahassan94@gmail.com>
2019-07-06 13:39:48 -0400

    Add Footer and Header

---
+++ front-end/src/components/TheHeader.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav>
+    <div>
+      <router-link :to="{ name: 'home' }">
+        Favorite
+      </router-link>
+      <ul>
+        <li>
+          <router-link
+                  active-class="active"
+                  exact
+                  :to="{ name: 'home' }"
+          >
+            Home
+          </router-link>
+        </li>
+        <li>
+          <router-link
+                  active-class="active"
+                  :to="{ name: 'favorite-edit' }"
+          >
+            <i class="ion-compose"></i>&nbsp;New Favorite
+          </router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+  export default {
+    name: 'RwvHeader',
+    computed: {
+      ...mapGetters(['currentUser'])
+    }
+  }
+</script>
---
+++ front-end/src/components/VFavoritePreview.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="article-preview">
+    <router-link :to="articleLink" class="preview-link">
+      <h1 v-text="favorite.title" />
+      <p v-text="favorite.description" />
+      <button class="btn btn-outline-danger btn-sm" @click="deleteFavorite">
+        <i class="ion-trash-a"></i> <span>&nbsp;Delete Favorite</span>
+      </button>
+      <router-link class="btn btn-sm btn-outline-secondary" :to="editArticleLink">
+        <i class="ion-edit"></i> <span>&nbsp;Edit Favorite</span>
+      </router-link>
+    </router-link>
+  </div>
+</template>
+
+<script>
+
+
+  export default {
+    name: "RwvArticlePreview",
+    components: {
+    },
+    props: {
+      favorite: { type: Object, required: true }
+    },
+    computed: {
+      articleLink() {
+        return {
+          name: "favorite",
+        };
+      },
+      editArticleLink() {
+        return { name: "favorite-edit", params: { slug: this.favorite.id } };
+      },
+    },
+    methods: {
+      async deleteFavorite() {
+      }
+    }
+  };
+</script>
