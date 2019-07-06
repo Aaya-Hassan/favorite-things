84be2416a96d6c5b87aa1522faf2b3986950e5f7
Aaya-Hassan <aayahassan94@gmail.com>
2019-07-06 13:38:14 -0400

    Add homepage

---
+++ front-end/src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home-page">
+    <div class="banner">
+      <div class="container">
+        <h1 class="logo-font"> Favorite </h1>
+      </div>
+    </div>
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-9">
+          <div class="feed-toggle">
+              <router-link
+                  :to="{ name: 'home' }"
+                  exact
+                  class="nav-link"
+                  active-class="active"
+                >
+                </router-link>
+          </div>
+          <router-view></router-view>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "home",
+};
+</script>
